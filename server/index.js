b3a6f4d4a0f6e01bb562d259fa426e03dd728185
Robel Tekle <robel.tekle@gmail.com>
2019-03-09 13:59:22 +0100

    init

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(3000, () => {
+  console.log('Listening at port 3000')
+})
