292cdd583d853a50d67d0f9f627ebc3e76d88cf6
Robel Tekle <robel.tekle@gmail.com>
2019-03-10 16:12:42 +0100

    rename token

--- server/src/Auth/index.js
+++ server/src/Auth/index.js
@@ -2,12 +2,12 @@ const User = require('../db/models/Users')
 const { errorSender } = require('../helpers/shortResponse')
 
 const auth = async (req, res, next) => {
-  const { chatapp_token } = req.headers
-  if (!chatapp_token) {
+  const { chatapp_token: token } = req.headers
+  if (!token) {
     res.redirect('/login')
     return
   }
-  await User.findOne({ token: chatapp_token }, (error, user) => {
+  await User.findOne({ token }, (error, user) => {
     if (user) {
       req.chatapp_user = user
       next()
